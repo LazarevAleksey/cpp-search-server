9b4809939030fe39f860b8875c69b038b35955dd
Aleksey <bins87@mail.ru>
2022-05-21 20:14:03 +0300

    Add files via upload

---
+++ document.cpp
@@ -0,0 +1,44 @@
+//Вставьте сюда своё решение из урока «‎Очередь запросов».‎
+#include <iostream>
+#include <string>
+
+//#include"search_server.h"
+#include "document.h"
+
+using namespace std;
+
+
+	Document::Document() = default;
+	Document::Document(int id, double relevance, int rating)
+		: id(id)
+		, relevance(relevance)
+		, rating(rating) {
+	}
+
+	std::ostream& operator<<(std::ostream& out, const Document& document) {
+		out << "{ "s
+			<< "document_id = "s << document.id << ", "s
+			<< "relevance = "s << document.relevance << ", "s
+			<< "rating = "s << document.rating << " }"s;
+		return out;
+	}
+
+	void PrintDocument(const Document& document) {
+		cout << "{ "s
+			<< "document_id = "s << document.id << ", "s
+			<< "relevance = "s << document.relevance << ", "s
+			<< "rating = "s << document.rating << " }"s << endl;
+	}
+
+	void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words, DocumentStatus status) {
+		cout << "{ "s
+			<< "document_id = "s << document_id << ", "s
+			<< "status = "s << static_cast<int>(status) << ", "s
+			<< "words ="s;
+		for (const std::string& word : words) {
+			cout << ' ' << word;
+		}
+		cout << "}"s << endl;
+	}
+
+	
